14ae430be37cb4ba3ce3a898ba12a69d885afda1
JasperJackalope <125604651+JasperJackalope@users.noreply.github.com>
2023-04-18 14:56:10 -0700

    Refinded API routes

--- models/index.js
+++ models/index.js
@@ -3,14 +3,27 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-
+// TODO:
 // Products belongsTo Category
-
 // Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+
+Product.belongsTo(Category, {
+  foreignKey: {
+    allowNull: false
+  }
+});
+Category.hasMany(Product);
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
